61289bf2c89d37e2265d9523d066688f5589edb7
brandoncansler <60812271+brandoncansler@users.noreply.github.com>
2020-03-30 13:17:50 -0500

    second commit
    added app code

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('Single Cheese');
+
+INSERT INTO burgers(burger_name)
+VALUES('Bacon Cheese');
+
+INSERT INTO burgers(burger_name)
+VALUES('Double Double');
+
+INSERT INTO burgers(burger_name)
+VALUES('Veggie Burger');
